0aa3892a942320de13aa0b54b750b5167789b7c5
ANCHANIK <cksdlr7446@gmail.com>
2022-07-22 23:41:35 +0900

    update main

--- src/components/main/MainSixBlock.js
+++ src/components/main/MainSixBlock.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { MainContext } from "../../contexts/MainContext";
 import { Table } from "../../styles/MainStyle"
-import Slider from "react-slick";
+// import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import DaySchedules from "./ScheduleTable/DaySchedules"
@@ -11,17 +11,17 @@ import WeekSchedules from "./ScheduleTable/WeekSchedules";
 const SchedulesTable = () => {
 
     const { selectDate } = useContext(MainContext);
-    const settings = {
-        // 슬라이드 옵션들
-        arrows: false,  // 화살표 표시
-        dots: false,  // 밑에 현재 페이지와 나머지 페이지 점으로 표시
-        infinite: false,  // 무한 반복
-        speed: 500,  // 넘기는 속도
-        slidesToShow: 1,  // 슬라이드에 보여지는 아이템 개수
-        slidesToScroll: 1,  // 슬라이드 넘기는 아이템 개수
-        autoplay: false,  // 자동 재생
-        autoplaySpeed: 0,  // 자동 재생 속도
-    };
+    // const settings = {
+    //     // 슬라이드 옵션들
+    //     arrows: false,  // 화살표 표시
+    //     dots: false,  // 밑에 현재 페이지와 나머지 페이지 점으로 표시
+    //     infinite: false,  // 무한 반복
+    //     speed: 500,  // 넘기는 속도
+    //     slidesToShow: 1,  // 슬라이드에 보여지는 아이템 개수
+    //     slidesToScroll: 1,  // 슬라이드 넘기는 아이템 개수
+    //     autoplay: false,  // 자동 재생
+    //     autoplaySpeed: 0,  // 자동 재생 속도
+    // };
 
     return (
         <Table>
