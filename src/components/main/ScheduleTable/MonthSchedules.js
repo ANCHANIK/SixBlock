35187dd08d3d9c27d6c8d6dc588e03eaa5bb73cb
cksdlr7446 <82303701+ANCHANIK@users.noreply.github.com>
2022-07-29 17:44:13 +0900

    Update MonthSchedules.js

--- src/components/main/ScheduleTable/MonthSchedules.js
+++ src/components/main/ScheduleTable/MonthSchedules.js
@@ -1,31 +1,12 @@
 import { useState } from "react";
-import { MonthlyTableStyle } from "../../../styles/MainStyle";
-import { Calendar } from "react-date-range";
-import * as locales from 'react-date-range/dist/locale';
-import 'react-date-range/dist/styles.css'; // main css file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-// import MonthLibrary from "./MonthLibrary";
 
 const MonthSchedules = () => {
 
-    const [locale, setLocale] = useState('us');
 
 
     return (
         <>
-            <div className="custom_calendar">
-                <Calendar
-                    // onChange={onDateClick}
-                    locale={locales[locale]}
-                    showMonthArrow={false}
-                    direction="horizontal"
-
-                    // date={date}
-                    // displayMode={1}
-                    // startDate={date}
-                    // weekdayDisplayFormat='' // 일 월 화 수 목 금 토
-                />
-            </div>
+            {/* <MonthLibrary /> */}
         </>
     )
 }
